084795bf83ec494998d06f6ca5acbbce4d856eed
João Olívio <joaoolivio16@gmail.com>
2023-05-26 11:34:42 -0300

    ss

--- src/f.sql
+++ src/f.sql
@@ -3,7 +3,11 @@ SELECT AVG(a.nota) AS media_avaliacoes
   FROM avaliacoes a
 INNER JOIN entradas e on a.entrada_id = e.id
 INNER JOIN promotor_fornecedores pf on e.promotor_fornecedor_id = pf.id
+<<<<<<< HEAD
 WHERE pf.promotor_id = ${req.body.id}
+=======
+WHERE pf.promotor_id = 1
+>>>>>>> 3fd8bac879cb57bcf12d87792c000f04842505c9
 ORDER BY a.created_at DESC
 LIMIT 3
 
@@ -13,7 +17,11 @@ SELECT COUNT(*) AS servicos_nao_concluidos
                 FROM avaliacoes a
               INNER JOIN entradas e on a.entrada_id = e.id
 			        INNER JOIN promotor_fornecedores pf on e.promotor_fornecedor_id = pf.id
+<<<<<<< HEAD
                WHERE pf.promotor_id = ${req.body.id}
+=======
+               WHERE pf.promotor_id = 1  --${req.body.id}
+>>>>>>> 3fd8bac879cb57bcf12d87792c000f04842505c9
               ORDER BY a.created_at DESC
               LIMIT 3
  )
