99ffbe076959a1e238a72e97c45125789f3071aa
João Olívio <joaoolivio16@gmail.com>
2023-06-16 09:58:21 -0300

    config

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,13 +1,13 @@
 // Configuração do bando de dados no ambiente de teste
-export const databaseConfig = {
-    dialect: 'sqlite',
-    storage: 'database.sqlite',
-    define: {
-      timestamps: true,
-      freezeTableName: true, //Não prularizar
-      underscored: true //Trabalr com _
-    }
-  };
+// export const databaseConfig = {
+//     dialect: 'sqlite',
+//     storage: 'database.sqlite',
+//     define: {
+//       timestamps: true,
+//       freezeTableName: true, //Não prularizar
+//       underscored: true //Trabalr com _
+//     }
+//   };
   
   /*
   // Configuração do bando de dados no ambiente de desenvolvimento
@@ -25,18 +25,22 @@ export const databaseConfig = {
   };
   */
   
-  /*
+
   // Configuração do bando de dados no ambiente de produção
   export const databaseConfig = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'scv-backend-node-sequelize',
+    host: 'dpg-ci65f7enqql3q38ebrc0-a.oregon-postgres.render.com',
+    username: 'scap_backend_node_sequelize_user',
+    password: 'UxSmKyPVMd91qgYBsppF2RBsfzoDhSuv',
+    database: 'scap_backend_node_sequelize',
     define: {
       timestamps: true,
       freezeTableName: true,
       underscored: true
+    },
+    dialectOptions: {
+      ssl: true
     }
   };
-  */
+
+
