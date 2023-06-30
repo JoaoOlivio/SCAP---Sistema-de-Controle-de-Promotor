484a691422c0fda515d621629dedf92053f65113
Diogo Oliveira de Lima <diogo.oliveira.lima1@gmail.com>
2023-06-30 11:37:25 -0300

    preparando para subir o projeto para o render

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,46 +1,46 @@
 //Configuração do bando de dados no ambiente de teste
-export const databaseConfig = {
-    dialect: 'sqlite',
-    storage: 'database.sqlite',
-    define: {
-      timestamps: true,
-      freezeTableName: true, //Não prularizar
-      underscored: true //Trabalr com _
-    }
-  };
+// export const databaseConfig = {
+//     dialect: 'sqlite',
+//     storage: 'database.sqlite',
+//     define: {
+//       timestamps: true,
+//       freezeTableName: true, //Não prularizar
+//       underscored: true //Trabalr com _
+//     }
+//   };
   
-  /*
-  // Configuração do bando de dados no ambiente de desenvolvimento
-  export const databaseConfig = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'scv-backend-node-sequelize',
-    define: {
-      timestamps: true,
-      freezeTableName: true,
-      underscored: true
-    }
-  };
-  */
   
-
-  // Configuração do bando de dados no ambiente de produção
+  //Configuração do bando de dados no ambiente de desenvolvimento
   // export const databaseConfig = {
   //   dialect: 'postgres',
-  //   host: 'dpg-ci65f7enqql3q38ebrc0-a.oregon-postgres.render.com',
-  //   username: 'scap_backend_node_sequelize_user',
-  //   password: 'UxSmKyPVMd91qgYBsppF2RBsfzoDhSuv',
-  //   database: 'scap_backend_node_sequelize',
+  //   host: 'localhost',
+  //   username: 'postgres',
+  //   password: 'postgres',
+  //   database: 'scv-backend-node-sequelize',
   //   define: {
   //     timestamps: true,
   //     freezeTableName: true,
   //     underscored: true
-  //   },
-  //   dialectOptions: {
-  //     ssl: true
   //   }
   // };
+  
+  
+
+  //Configuração do bando de dados no ambiente de produção
+  export const databaseConfig = {
+    dialect: 'postgres',
+    host: 'dpg-cifdnd95rnujc4s6cm9g-a.oregon-postgres.render.com',
+    username: 'scap_backend_node_sequelize_user',
+    password: 'Sw4Mma2jZWfPoN8deIGekxhmr7Bm7psz',
+    database: 'scap_backend_node_sequelize_obnh',
+    define: {
+      timestamps: true,
+      freezeTableName: true,
+      underscored: true
+    },
+    dialectOptions: {
+      ssl: true
+    }
+  };
 
 
