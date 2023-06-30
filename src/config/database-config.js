c07cc3cee34d2cf58e12332a22558711c938ffaf
Diogo Oliveira de Lima <diogo.oliveira.lima1@gmail.com>
2023-06-30 10:15:57 -0300

    ajustes de saidas inseridas no arquivo database

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,13 +1,13 @@
-// Configuração do bando de dados no ambiente de teste
-// export const databaseConfig = {
-//     dialect: 'sqlite',
-//     storage: 'database.sqlite',
-//     define: {
-//       timestamps: true,
-//       freezeTableName: true, //Não prularizar
-//       underscored: true //Trabalr com _
-//     }
-//   };
+//Configuração do bando de dados no ambiente de teste
+export const databaseConfig = {
+    dialect: 'sqlite',
+    storage: 'database.sqlite',
+    define: {
+      timestamps: true,
+      freezeTableName: true, //Não prularizar
+      underscored: true //Trabalr com _
+    }
+  };
   
   /*
   // Configuração do bando de dados no ambiente de desenvolvimento
@@ -27,20 +27,20 @@
   
 
   // Configuração do bando de dados no ambiente de produção
-  export const databaseConfig = {
-    dialect: 'postgres',
-    host: 'dpg-ci65f7enqql3q38ebrc0-a.oregon-postgres.render.com',
-    username: 'scap_backend_node_sequelize_user',
-    password: 'UxSmKyPVMd91qgYBsppF2RBsfzoDhSuv',
-    database: 'scap_backend_node_sequelize',
-    define: {
-      timestamps: true,
-      freezeTableName: true,
-      underscored: true
-    },
-    dialectOptions: {
-      ssl: true
-    }
-  };
+  // export const databaseConfig = {
+  //   dialect: 'postgres',
+  //   host: 'dpg-ci65f7enqql3q38ebrc0-a.oregon-postgres.render.com',
+  //   username: 'scap_backend_node_sequelize_user',
+  //   password: 'UxSmKyPVMd91qgYBsppF2RBsfzoDhSuv',
+  //   database: 'scap_backend_node_sequelize',
+  //   define: {
+  //     timestamps: true,
+  //     freezeTableName: true,
+  //     underscored: true
+  //   },
+  //   dialectOptions: {
+  //     ssl: true
+  //   }
+  // };
 
 
